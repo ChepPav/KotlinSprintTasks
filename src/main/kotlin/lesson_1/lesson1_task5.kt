86905d84ad5648de300142e1abfac92249a349a2
chepuh <pavlechep@gmail.com>
2025-11-23 20:43:25 +0400

    KS-1-5 refactored the task

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,11 +1,12 @@
 package lesson_1
 
+const val SIXTY = 60
 fun main() {
     val seconds = 6480
-    val minutes = seconds/60
-    val hours = minutes/60
+    val minutes = seconds / SIXTY
+    val hours = minutes / SIXTY
 
-    val remainSeconds = seconds%60
-    val remainMinute = minutes%60
-    println("%02d:%02d:%02d".format(hours, remainMinute, remainSeconds))
+    val remainingSeconds = seconds % 60
+    val remainingMinute = minutes % 60
+    println("%02d:%02d:%02d".format(hours, remainingMinute, remainingSeconds))
 }
