74a44f5518c02227441cdfd535402ea609db89a2
chepuh <pavlechep@gmail.com>
2025-11-23 14:42:15 +0400

    KST-1-3 сompleted the task

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,12 +1,14 @@
 package lesson_1
 
-fun main(){
+fun main() {
     val year = 1961
-    // Использую String так-как нельзя через Int инициализировать число начинающиеся с 0, если это не Double
     var hour  = "09"
     var minute = "07"
-    println("Год полета - $year\nВзлет - $hour:$minute")
+    println("Год полета - $year")
+    print("Взлет - $hour:")
+    println(minute)
     hour = "10"
     minute = "55"
-    println("Посадка - $hour:$minute")
+    print("Посадка - $hour:")
+    print(minute)
 }
