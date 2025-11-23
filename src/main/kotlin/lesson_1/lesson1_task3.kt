9c1def874b6446449f05b09415f82285a96d37a6
chepuh <pavlechep@gmail.com>
2025-11-23 20:27:19 +0400

    KS-1-3 refactored the task

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,13 +2,12 @@ package lesson_1
 
 fun main() {
     val year = 1961
-    var hour  = "09"
+    var hour = "09"
     var minute = "07"
     println("Год полета - $year")
-    print("Взлет - $hour:")
-    println(minute)
+    println("Час взлета - $hour")
+    println("Минуты взлета - $minute")
     hour = "10"
     minute = "55"
-    print("Посадка - $hour:")
-    print(minute)
+    print("Посадка - $hour:$minute")
 }
