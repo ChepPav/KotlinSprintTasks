90639c1a7aa2498310e1d8869d3b8dd4fc9cc3b2
chepuh <pavlechep@gmail.com>
2025-11-25 12:47:33 +0400

    KS-3-1 complete the task

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,9 @@
+package lesson_3
+
+const val USER_NAME = "Oleg"
+fun main() {
+    var userTime = "день"
+    println("Добрый $userTime $USER_NAME !")
+    userTime = "вечер"
+    println("Добрый $userTime $USER_NAME !")
+}
