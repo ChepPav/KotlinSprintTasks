f29ae2a8d20e1d5efbf7fbf9d693254a2b361db4
chepuh <pavlechep@gmail.com>
2025-11-25 13:19:45 +0400

    KS-3-3 complete the task

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,20 @@
+package lesson_3
+
+const val NUM = 8
+fun main() {
+
+    println(
+        """"
+        |$NUM x 1 = ${NUM * 1}
+        |$NUM x 2 = ${NUM * 2}
+        |$NUM x 3 = ${NUM * 3}
+        |$NUM x 4 = ${NUM * 4}
+        |$NUM x 5 = ${NUM * 5}
+        |$NUM x 6 = ${NUM * 6}
+        |$NUM x 7 = ${NUM * 7}
+        |$NUM x 8 = ${NUM * 8}
+        |$NUM x 9 = ${NUM * 9}
+        |$NUM x 10 = ${NUM * 10}
+    """.trimMargin()
+    )
+}
