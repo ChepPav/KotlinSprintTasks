b873daab0c67dba7546cf7bfbaf5080fc22dbf94
chepuh <pavlechep@gmail.com>
2025-11-26 12:19:39 +0400

    KS-2-4 refactor the task

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -2,11 +2,11 @@ package lesson_2
 
 import kotlin.math.pow
 
-const val AMOUNT_MONEY = 70_000
-const val PERCENT_CONTRIBUTION: Double = 16.7 / 100
-const val YEARS = 20
-
 fun main() {
-    val percentYear = ((1 + PERCENT_CONTRIBUTION).pow(YEARS))
-    println("%.3f".format(AMOUNT_MONEY * percentYear))
+    val amountMoney = 70_000
+    val percentContribution: Double = 16.7 / 100
+    val years = 20
+
+    val percentYear = ((1 + percentContribution).pow(years))
+    println("%.3f".format(amountMoney * percentYear))
 }
