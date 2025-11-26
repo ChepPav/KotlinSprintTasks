080c4bacdd1b80cae3db80e56b968c95ac42aa3a
chepuh <pavlechep@gmail.com>
2025-11-26 12:14:37 +0400

    KS-2-3 refactor the task

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,17 +1,18 @@
 package lesson_2
 
-const val TRAIN_DEPARTURE_HOURS = 9
-const val TRAIN_DEPARTURE_MINUTE = 39
-const val TRAIN_TRAVEL_MINUTE = 457
 const val SIXTY = 60
 
 fun main() {
-    val totalMinuts = TRAIN_TRAVEL_MINUTE + TRAIN_DEPARTURE_MINUTE
-    val remainingMinuteInWay = totalMinuts % SIXTY
+    val trainDepartureHours = 9
+    val trainDepartureMinute = 39
+    val trainTravelMinute = 457
 
-    val hoursInWay = totalMinuts / SIXTY
-    val arravingHours = TRAIN_DEPARTURE_HOURS + hoursInWay
+    val totalMinute = trainTravelMinute + trainDepartureMinute
+    val remainingMinuteInWay = totalMinute % SIXTY
 
-    println("%02d:%02d".format(arravingHours, remainingMinuteInWay))
+    val hoursInWay = totalMinute / SIXTY
+    val arrivalHours = trainDepartureHours + hoursInWay
+
+    println("%02d:%02d".format(arrivalHours, remainingMinuteInWay))
 
 }
