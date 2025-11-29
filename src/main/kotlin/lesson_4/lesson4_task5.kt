c5e98927501161398283ed24c4039c2ae3b390f9
chepuh <pavlechep@gmail.com>
2025-11-29 21:17:52 +0400

    KS-4-5 refactor the task

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -5,26 +5,28 @@ const val MIN_CREW_NUMBERS = 55
 const val MAX_CREW_NUMBERS = 70
 const val PROVISIONS = 50
 const val GOOD_WEATHER = true
+
 fun main() {
     println("Отсутствую ли на корабль повреждения? Ввести \"true\" или \"false\"")
-    val actualShipCondition = readln().toBoolean()
+    val hasNoDamage = readln().toBoolean()
     println("Количество экипажа? Ввести число")
     val actualCrewNumber = readln().toInt()
     println("Количество провизии? Ввести число")
     val actualProvisions = readln().toInt()
     println("Хорошая ли погода? Ввести \"true\" или \"false\"")
-    val actualWether = readln().toBoolean()
+    val isWeatherGood = readln().toBoolean()
+
     println(
         """
         Может ли корабль выйти в плавание? 
         ${
-            actualShipCondition == SHIP_CONDITION &&
+            hasNoDamage == SHIP_CONDITION &&
                     actualCrewNumber in MIN_CREW_NUMBERS..MAX_CREW_NUMBERS &&
-                    actualProvisions > PROVISIONS &&
-                    (actualWether == GOOD_WEATHER || actualWether != GOOD_WEATHER) ||
-                    ((actualShipCondition == SHIP_CONDITION || actualShipCondition != SHIP_CONDITION) &&
+                    actualProvisions > PROVISIONS ||
+                    (hasNoDamage != SHIP_CONDITION &&
                             actualCrewNumber == MAX_CREW_NUMBERS &&
-                            actualProvisions >= PROVISIONS)
+                            actualProvisions >= PROVISIONS &&
+                            isWeatherGood == GOOD_WEATHER)
         }
     """.trimIndent()
     )
