e3db7c8168a5ca5fa7ae15c66d88c3a088f1edce
chepuh <pavlechep@gmail.com>
2025-11-29 21:09:58 +0400

    KS-4-3 refactor the task

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,16 +1,18 @@
 package lesson_4
 
-const val GOOD_WEATHER = "Солнечно"
-const val AWNING_OPEN = "Открыт"
+const val GOOD_WEATHER = true
+const val AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
 const val BAD_SEASON = "Зима"
+
 fun main() {
-    val weatherToday = "Солнечно"
-    val awning = "Открыт"
+    val isSunnyWeather = true
+    val isAwningOpen = true
     val airHumidity = 20
     val season = "Зима"
+
     println(
         "Благоприятные ли условия сейчас для роста бобовых? " +
-                "${weatherToday == GOOD_WEATHER && awning == AWNING_OPEN && airHumidity == AIR_HUMIDITY && season !== BAD_SEASON}"
+                "${isSunnyWeather == GOOD_WEATHER && isAwningOpen == AWNING_OPEN && airHumidity == AIR_HUMIDITY && season != BAD_SEASON}"
     )
 }
