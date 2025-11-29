22bdbf2b89dadd9c218640857465c458072c73e8
chepuh <pavlechep@gmail.com>
2025-11-29 21:02:50 +0400

    KS-4-1 refactor the task

--- src/main/kotlin/lesson_4/lesson4_task1.kt
+++ src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -1,10 +1,11 @@
 package lesson_4
 
 const val TABLES = 13
+
 fun main() {
     val bookingToday = 13
     val bookingTomorrow = 9
-    println("Доступность столиков на сегодня: ${TABLES > bookingToday}")
-    println("Доступность столиков на сегодня: ${TABLES > bookingTomorrow}")
 
+    println("Доступность столиков на сегодня: ${bookingToday < TABLES} \n"+
+            "Доступность столиков на сегодня: ${bookingTomorrow < TABLES}")
 }
